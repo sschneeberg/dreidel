2b3a01d8e392d0064ae17cf8d2477163ef3176e0
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-12 13:57:28 -0500

    Build spinner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import gimmel from './imgs/gimmel.png';
+import shin from './imgs/shin.png';
+import hay from './imgs/hay.png';
+import nun from './imgs/nun.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const imgs = [gimmel, hay, shin, nun];
+
+    const [img, setImg] = useState(imgs[0]);
+
+    const animation = () => {
+        const spin = setInterval(function () {
+            pickImg();
+        }, 80);
+        setTimeout(function () {
+            clearInterval(spin);
+        }, 1400);
+    };
+
+    const pickImg = () => {
+        let i = Math.floor(Math.random() * 4);
+        setImg(imgs[i]);
+    };
+
+    return (
+        <div className="App">
+            <h1>
+                Happy Virtual <br />
+                Hannukkah
+            </h1>
+            <div className="dreidel">
+                <div className="stem"></div>
+                <div className="body">
+                    <img src={img} alt="" />
+                </div>
+                <div className="point"></div>
+            </div>
+            <button type="button" onClick={animation}>
+                SPIN!
+            </button>
+        </div>
+    );
 }
 
 export default App;
